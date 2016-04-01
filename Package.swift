183a7fe3cdd1eaa83b1829836bf11c1e57d6027d
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2016-04-01 15:50:31 -0400

    WIP

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/ketzusaka/Hummingbird", majorVersion: 1)
     ],
     exclude: [
         "XcodeProject",
